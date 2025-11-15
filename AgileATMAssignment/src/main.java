91f3381baec59dbcbea89260279eda0a3d46ca67
Technado4 <89406074+Technado4@users.noreply.github.com>
2025-11-15 20:15:07 +0000

    Admin Panel

--- AgileATMAssignment/src/main.java
+++ AgileATMAssignment/src/main.java
@@ -5,7 +5,7 @@ import java.io.ObjectOutputStream;
 import java.util.Scanner;
 
 public class main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ValidateAccountExceptionHandler {
 		AccountManager account = new AccountManager();
 	    Scanner sc = new Scanner(System.in);
 	
@@ -45,6 +45,8 @@ public class main {
 
         System.out.println("Welcome, " + current.getUsername() + "!");
         boolean runningAtm = true;
+        Account selectedAccount = current;
+        
         
 		while (runningAtm) {
 	        System.out.println("\n--- Menu ---");
@@ -52,6 +54,16 @@ public class main {
 	        System.out.println("2. Deposit");
 	        System.out.println("3. Check Balance");
 	        System.out.println("4. Exit");
+	        if (current.isAdmin())
+	        {
+	        	System.out.println("\n--- Admin Panel ---");
+		        System.out.println("5. Select Account (Account Selected:"+selectedAccount.getUsername()+")");
+		        System.out.println("6. Change Username");
+		        System.out.println("7. Change PIN");
+		        System.out.println("8. Toggle Admin");
+		        System.out.println("9. Freeze Account");
+		        System.out.println("0. Delete Account");
+	        }
 	        System.out.print("Choose: ");
 	        int choice = sc.nextInt();
 	        switch (choice) {
@@ -81,6 +93,80 @@ public class main {
                 }
                 System.out.println("Thank you for using the ATM!");
                 break;
+            case 5:
+            	if (current.isAdmin())
+            	{
+            		System.out.println("Enter account to edit: ");
+                    String choice2 = sc.next();
+                    for (Account a : account.getAccounts()) {
+                        if (a.getUsername().equals(choice2)) {
+                            selectedAccount = a;
+                            System.out.println("Account set to: "+selectedAccount.getUsername());
+                        }
+                    }
+            	}
+            	else
+            	{
+            		System.out.println("Invalid choice.");
+            	}
+            	break;
+            case 6:
+            	if (current.isAdmin())
+            	{
+            		System.out.println("Enter new username: ");
+                    String choice2 = sc.next();
+                    selectedAccount.setUsername(choice2);
+                    System.out.println("Username set to: "+selectedAccount.getUsername());
+            	}
+            	else
+            	{
+            		System.out.println("Invalid choice.");
+            	}
+            	break;
+            case 7:
+            	if (current.isAdmin())
+            	{
+            		System.out.println("Enter new PIN: ");
+                    String choice2 = sc.next();
+                    selectedAccount.setPin(choice2);
+                    System.out.println("PIN set to: "+selectedAccount.getPin());
+            	}
+            	else
+            	{
+            		System.out.println("Invalid choice.");
+            	}
+            	break;
+            case 8:
+            	if (current.isAdmin())
+            	{
+            		selectedAccount.setAdmin(!current.isAdmin());
+            		System.out.println("Admin Toggled!.");
+            	}
+            	else
+            	{
+            		System.out.println("Invalid choice.");
+            	}
+            	break;
+            case 9:
+            	if (current.isAdmin())
+            	{
+            		System.out.println("Not Implemented");
+            	}
+            	else
+            	{
+            		System.out.println("Invalid choice.");
+            	}
+            	break;
+            case 0:
+            	if (current.isAdmin())
+            	{
+            		System.out.println("Not Implemented");
+            	}
+            	else
+            	{
+            		System.out.println("Invalid choice.");
+            	}
+            	break;
             default:
                 System.out.println("Invalid choice.");
         }
